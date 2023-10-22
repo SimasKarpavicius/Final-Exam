daacd2164f7e3b3d8e377bddf3f81995e9b17107
Simas Karpavicius <karpavicius.simas020@gmail.com>
2023-10-22 17:31:26 +0300

    updates

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const mongoose  = require("mongoose");
 require("dotenv").config();
 const RegisterModel = require('./models/Register')
+const PostModel = require('./models/Register')
 
 const app = express()
 app.use(cors())
@@ -11,6 +12,11 @@ app.use(express.json())
 const port = process.env.PORT || 8080;
 const URI = process.env.DB_CONNECTION_STRING;
 const client = mongoose.connect(URI);
+
+// const users = [];
+// const threadList = [];
+
+const generateID = () => Math.random().toString(36).substring(2, 10);
 
 app.post("/login", (req, res)=>{
   const {email, password} = req.body;
@@ -28,18 +34,26 @@ app.post("/login", (req, res)=>{
 })
 
 app.post('/register', (req, res) =>{
-  const { name, email, password} = req.body;
+  const { email, password} = req.body;
   RegisterModel.findOne({email: email})
   .then(user =>{
     if(user) {
       res.json("Already have an account")
     } else {
-      RegisterModel.create({name: name, email: email, password: password})
+      RegisterModel.create({email: email, password: password})
       .then(result =>res.json("Account created"))
       .catch(err => res.status(500).json(err));
     }
   }).catch(err => res.status(500).json(err));
 })
+
+app.post("/api/create/thread", async (req, res) => {
+  const {thread, userId} = req.body;
+  const threadId = generateID();
+
+  console.log({thread, userId, threadId});
+}
+)
 
 app.listen(port, () => {
   console.log(`server is running on http://localhost:${port}`)
--- server/models/Register.js
+++ server/models/Register.js
@@ -1,10 +1,17 @@
 const mongoose = require("mongoose");
 
 const RegisterSchema = new mongoose.Schema ({
-    name: String,
     email: String,
     password: String,
 })
 
 const RegisterModel = mongoose.model("register", RegisterSchema)
 module.exports = RegisterModel;
+
+const PostSchema = new mongoose.Schema ({
+    title: String,
+    description: String,
+})
+
+const PostModel = mongoose.model("post", PostSchema)
+module.exports = PostModel;
